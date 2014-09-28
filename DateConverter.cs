9ad32c2547c572027e05d0011ee24ff9476aedf0
JeffLeFevre <jeff.lefevre@gmail.com>
2014-09-28 17:49:43 -0600

    Updated Version
    This is the more recent version of SuperList

---
+++ DateConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace SuperList.WPF
+{
+    class DateConverter : IValueConverter
+    {
+        //This forces the UI to display the date only, as opposed to date and time.
+        //Used on Item Created Date DataGrid Column
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            DateTime date = (DateTime)value;
+            return date.ToString("d");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string strValue = value as string;
+            DateTime resultDateTime;
+            if (DateTime.TryParse(strValue, out resultDateTime))
+            {
+                return resultDateTime;
+            }
+
+            throw new Exception("Unable to convert date to string.");
+        }
+    }
+}
